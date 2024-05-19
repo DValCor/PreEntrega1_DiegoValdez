4bab3f78596c098fe205ac881d474300bbb3a8e0
Diego Valdez Cor <dnvaldezcor@gmail.com>
2024-05-19 18:39:12 -0300

    Creación de un ciclo for simple

--- ciclo.js
+++ ciclo.js
@@ -1,9 +1,3 @@
-
-document.getElementById("calcular").addEventListener("click", function() {
-    let productos = document.querySelectorAll("#lista-productos input[type='checkbox']:checked");
-    let costoTotal = 0;
-    for (let i = 0; i < productos.length; i++) {
-        costoTotal += parseInt(productos[i].value);
-    }
-    document.getElementById("resultado").textContent = "El costo total de los productos seleccionados es: USD" + costoTotal;
-});
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
